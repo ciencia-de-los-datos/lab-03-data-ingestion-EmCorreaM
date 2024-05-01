f7659ae76b690481907bb34e2aa702a86345ad65
EmCorreaM <emcorream@unal.edu.co>
2024-05-01 14:35:50 -0500

    Intento 1 - Lab DataIngestion

--- pregunta.py
+++ pregunta.py
@@ -13,11 +13,28 @@ import pandas as pd
 
 
 def ingest_data():
-    
-    ruta_archivo = 'clusters_report.txt'
-    df = pd.read_csv(ruta_archivo, sep='\s+', skiprows=2, engine='python') # Añadir engine='python' para evitar el error de tokenizing
-    df.columns = ['Cluster', 'Cantidad_de_palabras_clave', 'Porcentaje_de_palabras_clave', 'Principales_palabras_clave']
+    with open('clusters_report.txt', 'r') as archivo:
+        lineas = archivo.readlines()
+
+    datos = []
+
+    for linea in lineas:
+        if not linea.startswith('-'):
+            columnas = linea.split()
+
+            if len(columnas) > 1:
+                cluster = columnas[0]
+                cantidad_palabras_clave = columnas[1]
+                porcentaje_palabras_clave = columnas[2]
+                palabras_clave = ' '.join(columnas[3:]).replace(',', ', ')
+
+                datos.append([cluster, cantidad_palabras_clave, porcentaje_palabras_clave, palabras_clave])
+
+    df = pd.DataFrame(datos, columns=['cluster', 'cantidad_palabras_clave', 'porcentaje_palabras_clave', 'palabras_clave'])
+
     df.columns = df.columns.str.lower().str.replace(' ', '_')
-    df['Principales_palabras_clave'] = df['Principales_palabras_clave'].str.replace('\s+', ' ').str.strip()
+
     return df
 
+df = ingest_data()
+print(df)
