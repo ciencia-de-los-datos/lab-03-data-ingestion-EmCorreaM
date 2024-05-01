de3eb2858a7eae534f1dcab559e0a24ae34e1c55
EmCorreaM <emcorream@unal.edu.co>
2024-05-01 14:01:41 -0500

    Intento 1 - Lab DataIngestion

--- pregunta.py
+++ pregunta.py
@@ -14,8 +14,9 @@ import pandas as pd
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
-
+    ruta_archivo = 'clusters_report.txt'
+    df = pd.read_csv(ruta_archivo, sep='\s+', skiprows=2)
+    df.columns = ['Cluster', 'Cantidad_de_palabras_clave', 'Porcentaje_de_palabras_clave', 'Principales_palabras_clave']
+    df.columns = df.columns.str.lower().str.replace(' ', '_')
+    df['Principales_palabras_clave'] = df['Principales_palabras_clave'].str.replace('\s+', ' ').str.strip()
     return df
