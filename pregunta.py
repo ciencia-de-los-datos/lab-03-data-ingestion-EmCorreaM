79da0d507b57ec7589bfed273a26f34a966d530e
EmCorreaM <emcorream@unal.edu.co>
2024-05-02 18:18:40 -0500

    Intento 2 - Lab DataIngestion

--- pregunta.py
+++ pregunta.py
@@ -12,31 +12,36 @@ import pandas as pd
 
 def ingest_data():
     with open("clusters_report.txt", 'r') as file:
-        lineas = file.readlines()[4:]
+        lines = file.readlines()[4:]
 
     data = []
-    fila_actual = ''
-    for linea in lineas:
-        linea_strip = linea.strip()
-        if not linea_strip:
-            if fila_actual:
-                data.append(fila_actual.strip())
-                fila_actual = ''
+    current_row = ''
+    for line in lines:
+        line_strip = line.strip()
+        if not line_strip:
+            if current_row:
+                data.append(current_row)
+                current_row = ''
         else:
-            fila_actual += ' ' + linea_strip
+            current_row += ' ' + line_strip
 
-    if fila_actual:
-        data.append(fila_actual.strip())
+    if current_row:
+        data.append(current_row)
 
     df = pd.DataFrame(data, columns=['Columna'])
     df['Columna'] = df['Columna'].str.replace(r'\s+%', '', regex=True)
-    df['Columna'] = df['Columna'].str.strip().apply(lambda x: x if x.isdigit() else x.strip())
-    df = df['Columna'].str.replace(r'\s+', ' ').str.split(' ', n=3, expand=True)
+    df['Columna'] = df['Columna'].apply(lambda x: x.strip() if x.isnumeric() else x)
+    df = df.replace(r'\s+', ' ', regex=True)
+    df['Columna'] = df['Columna'].str.replace(' ', '', 1)
+    df = df['Columna'].str.split(' ', n=3, expand=True)
 
     df.columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave']
     df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].str.replace(',', '.')
     df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace('.', '')
-    df[['cantidad_de_palabras_clave', 'cluster']] = df[['cantidad_de_palabras_clave', 'cluster']].astype(int)
+    df['cantidad_de_palabras_clave'] = df['cantidad_de_palabras_clave'].astype(int)
+    df['cluster'] = df['cluster'].astype(int)
     df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].astype(float)
 
     return df
+df = ingest_data()
+print(df.cluster.to_list())
